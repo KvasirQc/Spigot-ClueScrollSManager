2a67acbc2bc8d61b46207af070cad49283cb367c
Raphael Vigneault Ð. <68454661+Izocel@users.noreply.github.com>
2023-11-13 22:17:54 +0000

    base

--- src/main/java/main/ClueScrollManager.java
+++ src/main/java/main/ClueScrollManager.java
@@ -5,12 +5,12 @@ import java.util.logging.Logger;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MyPlugin extends JavaPlugin implements Listener {
+public final class ClueScrollManager extends JavaPlugin implements Listener {
 
     private Logger logger;
 
-    public MyPlugin() {
-        this.logger = Logger.getLogger("MyPlugin");
+    public ClueScrollManager() {
+        this.logger = Logger.getLogger("ClueScrollManager");
         saveDefaultConfig();
     }
 
