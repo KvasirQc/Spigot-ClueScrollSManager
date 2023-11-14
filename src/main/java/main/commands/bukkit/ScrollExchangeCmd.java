8d8540b639a07fa0167bd21f5a5ac7cea4f8ba85
Raphael Vigneault Ð <triangle1232009@hotmail.com>
2023-11-13 22:21:13 -0500

    temp

--- src/main/java/main/commands/bukkit/ScrollExchangeCmd.java
+++ src/main/java/main/commands/bukkit/ScrollExchangeCmd.java
@@ -1,6 +1,7 @@
 package main.commands.bukkit;
 
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 import com.electro2560.dev.cluescrolls.api.ClueScrollsAPI;
 
@@ -17,19 +18,32 @@ public class ScrollExchangeCmd extends PlayerBaseCmd {
   public void execute() {
     try {
       final ClueScrollsAPI api = plugin.getScrollAPI();
-      final int tier = Integer.parseInt(args[0]);
-      final int count = Integer.parseInt(args[1]);
+      final String tier = args[0];
+      final int amount = Integer.parseInt(args[1]);
 
-      if(true) {
-        logger.info(String.format("Unable to make scroll exchange. [Not enough papers... Tier: %1$s | Amount: %2$s]", tier, count));
+      var tiers = api.getTiers();
+      if (!tiers.contains(tier) || amount < 1) {
+        logger
+            .info(String.format("Unable to make scroll exchange. [Invalid amount or tier... Tier: %1$s | Amount: %2$s]",
+                tier, amount));
         return;
       }
-      
-    } catch (Exception e) {
-      logger.warning("Unable to make scroll exchange.");
-    }
 
-    EconomyManager.depositPlayer(player, 15);
+      int findCount = 0;
+      var playerItems = player.getInventory().getContents();
+
+      for (ItemStack itemStack : playerItems) {
+        if (itemStack.getAmount() == 1 && api.getScrollTier(itemStack).equals(tier)) {
+          itemStack.setAmount(0);
+          findCount++;
+        }
+      }
+
+      EconomyManager.depositPlayer(player, 1 * findCount);
+
+    } catch (Exception e) {
+      logger.warning("Unknown error ocurred during scroll exchange.");
+    }
   }
 
 }
