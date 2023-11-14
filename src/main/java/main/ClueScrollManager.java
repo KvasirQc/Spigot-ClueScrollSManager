589e823f6bc63e4fd06ded80d0007f8fd040874f
Raphael Vigneault Ð <triangle1232009@hotmail.com>
2023-11-13 20:58:54 -0500

    ...

--- src/main/java/main/ClueScrollManager.java
+++ src/main/java/main/ClueScrollManager.java
@@ -1,22 +1,69 @@
 package main;
 
+import java.net.http.WebSocket;
 import java.net.http.WebSocket.Listener;
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.electro2560.dev.cluescrolls.api.ClueScrollsAPI;
+
 public final class ClueScrollManager extends JavaPlugin implements Listener {
 
     private Logger logger;
+    private ClueScrollsAPI scrollAPI;
 
     public ClueScrollManager() {
         this.logger = Logger.getLogger("ClueScrollManager");
         saveDefaultConfig();
     }
 
+    public final String getPluginInfos(boolean toConsole) {
+        final StringBuilder sb = new StringBuilder();
+        final String devName = toConsole
+                ? "@Izo"
+                : "<@272924120142970892>";
+
+        sb.append(String.format(this.getName() + " - v1.1.0\n"));
+        sb.append(String.format(devName + "\n"));
+
+        return sb.toString();
+    }
+
+    public String getFiglet() {
+        return """
+                 _____   _                _____                _ _           ___  ___
+                /  __ \\| |              /  ___|              | | |          |  \\/  |
+                | /  \\/| |    _   _  ___\\ `--.  ___ _ __ ___ | | |  ______  | .  . | __ _ _ __   __ _  __ _  ___ _ __
+                | |    | |   | | | |/ _ \\`--. \\/ __| '__/ _ \\| | | |______| | |\\/| |/ _` | '_ \\ / _` |/ _` |/ _ \\ '__|
+                | \\__/\\| |___| |_| |  __/\\__/ / (__| | | (_) | | |          | |  | | (_| | | | | (_| | (_| |  __/ |
+                 \\____/\\_____/\\__,_|\\___\\____/ \\___|_|  \\___/|_|_|          \\_|  |_/\\__,_|_| |_|\\__,_|\\__, |\\___|_|
+                                                                                                       __/ |
+                                                                                                      |___/
+                """
+                + "\n" + this.getPluginInfos(true);
+    }
+
     @Override
     public final void onEnable() {
-        logger.info("Hello world !!!");
+        try {
+            new EconomyManager(this);
+            logger.info("LOADED: EconomyManager *vaultApi");
+
+            this.scrollAPI = ClueScrollsAPI.getInstance();
+            logger.info("LOADED: ClueScrollsAPI instance");
+
+            BukkitManager.registerEvents(this);
+            BukkitManager.registerCommands(this);
+
+            logger.info(this.getFiglet());
+        } catch (Exception e) {
+            logger.severe(e.getMessage());
+        }
+    }
+
+    public ClueScrollsAPI getScrollAPI() {
+        return scrollAPI;
     }
 
     @Override
---
+++ src/main/java/main/EconomyManager.java
@@ -0,0 +1,49 @@
+package main;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+
+public class EconomyManager {
+    private static Server server;
+    private static Economy vaultEconomy;
+    private static Logger logger;
+
+    public EconomyManager(ClueScrollManager plugin) {
+        logger = Logger.getLogger("WDMC:" + this.getClass().getSimpleName());
+
+        if (plugin.getServer().getPluginManager().getPlugin("Vault") == null) {
+            return;
+        }
+
+        EconomyManager.server = plugin.getServer();
+        RegisteredServiceProvider<Economy> vault = server.getServicesManager().getRegistration(Economy.class);
+        vaultEconomy = vault.getProvider();
+    }
+
+    public static EconomyResponse depositPlayer(Player player, double amount) {
+        logger.info(
+                String.format("Vault Player Currency --> Balance: %1$s | Depositing: %2$s | Player: %3$s",
+                        getPlayerBalance(player), amount, player.getName()));
+        return vaultEconomy.depositPlayer(player, amount);
+    }
+
+    public static EconomyResponse withdrawPlayer(Player player, double amount) {
+        logger.info(
+                String.format("Vault Player Currency --> Balance: %1$s | Withdrawing: %2$s | Player: %3$s",
+                        amount, player.getName()));
+        return vaultEconomy.withdrawPlayer(player, amount);
+    }
+
+    public static double getPlayerBalance(Player player) {
+        final double balance = vaultEconomy.getBalance(player);
+        logger.info(String.format("Vault Player Currency --> Balance: %1$s | Player: %2$s", balance, player.getName()));
+        return balance;
+    }
+
+}
